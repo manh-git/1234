7c1fc90ef091d174131aaabd70310850a7fcf6b8
manh-git <vuanh0038@gmail.com>
2025-04-18 00:42:04 +0700

    run.py

--- bot/deep_learning/vision_input/run.py
+++ bot/deep_learning/vision_input/run.py
@@ -25,11 +25,11 @@ def main():
     
     game = Game()
     dodgeMethod = {
-    "Furthest Safe Direction": lambda: GameBot(game, DodgeMethod.FURTHEST_SAFE_DIRECTION),
-    "Least Danger": lambda: GameBot(game, DodgeMethod.LEAST_DANGER_PATH),
-    "Least Danger Advanced": lambda: GameBot(game, DodgeMethod.LEAST_DANGER_PATH_ADVANCED),
-    "Opposite Threat Direction": lambda: GameBot(game, DodgeMethod.OPPOSITE_THREAT_DIRECTION),
-    "Random Safe Zone": lambda: GameBot(game, DodgeMethod.RANDOM_SAFE_ZONE),
+    "Furthest Safe Direction": lambda: BaseBot(game, DodgeMethod.FURTHEST_SAFE_DIRECTION),
+    "Least Danger": lambda: BaseBot(game, DodgeMethod.LEAST_DANGER_PATH),
+    "Least Danger Advanced": lambda: BaseBot(game, DodgeMethod.LEAST_DANGER_PATH_ADVANCED),
+    "Opposite Threat Direction": lambda: BaseBot(game, DodgeMethod.OPPOSITE_THREAT_DIRECTION),
+    "Random Safe Zone": lambda: BaseBot(game, DodgeMethod.RANDOM_SAFE_ZONE),
 }
     runner = BenchmarkRunner()
     save_path= "/content/drive/MyDrive/game_ai/benchmark_results.csv"
